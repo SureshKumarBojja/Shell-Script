901ad315f7993df84a06440baea2d646a039779c
SureshKumar <sureshtechie69@gmail.com>
2025-07-13 14:56:20 +0530

    this is a type of variables

--- 06-Arrays.sh
+++ 06-Arrays.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 FRUITS=("APPLE" "ORANGE" "GRAPES" "SAPOTA" "PINE APPLE" "BANANA")
-echo "1st: $FRUITS[0]"
-echo "2nd: $FRUITS[1]"
-echo "first 3: $FRUITS[0:4]"
-echo "step: $FRUITS[0::-1]"
-echo "All: $FRUITS[@]"
+echo "1st: ${FRUITS[0]}"
+echo "2nd: ${FRUITS[1]}"
+echo "first 3: ${FRUITS[0:4]}"
+echo "step: ${FRUITS[0::-1]}"
+echo "All: ${FRUITS[@]}"
