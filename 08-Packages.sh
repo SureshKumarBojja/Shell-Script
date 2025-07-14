9799bab7cc263f442bb5a2ab4f67fa1879d110d6
SureshKumar <sureshtechie69@gmail.com>
2025-07-14 12:37:45 +0530

    this is a type of variables

--- 08-Packages.sh
+++ 08-Packages.sh
@@ -8,32 +8,38 @@ N="\e[0m"
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then 
-        echo -e "$2...$R FAILED $N"
+    then
+        echo -e "$2 ... $R FAILED $N"
     else
-        echo -e "$2...$g SUCCESFULL $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-    echo -e "$R ERROR $N:: Be a root user"
-    exit 1
-else 
-     echo -e "you are a $G ROOT $N user"
-fi
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+#echo "All arguments passed: $@"
+# git mysql postfix net-tools
+# package=git for first time
 
 for package in $@
-do 
-    yum list insatlled $package &>> $LOGFILE
-    if [ $? -ne 0 ]
+do
+    yum list installed $package &>> $LOGFILE #check installed or not
+    if [ $? -ne 0 ] #if not installed
     then
-        yum install $package -y &>> $LOGFILE
-        VALIDATE $? "insatlling $package"
+        yum install $package -y &>> $LOGFILE # install the package
+        VALIDATE $? "Installation of $package" # validate
     else
-        echo -e "$package is already installed ...$Y SKIPPING $N"
+        echo -e "$package is already installed ... $Y SKIPPING $N"
     fi
 done
